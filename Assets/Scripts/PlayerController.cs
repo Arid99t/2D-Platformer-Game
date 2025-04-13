5ddf44246340d1680f6773580d42bcf8f8cb6fd2
Arindam Dutta <arindam.dutta99999@gmail.com>
2025-04-13 22:10:27 +0100

    Added PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    private void Awake()
+    {
+        Debug.Log("PLayer is awake");
+    }
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {           
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
